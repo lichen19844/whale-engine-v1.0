fa516435b81ffdecd0f38cce69b4b4ba7fa96187
lichen <lichen19844@gmail.com>
2017-01-13 00:44:09 -0800

    implement part4

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -28,7 +28,7 @@ class Admin::JobsController < ApplicationController
 
   def update
     @job = Job.find(params[:id])
-    if @job.update
+    if @job.update(job_params)
       redirect_to admin_jobs_path, notice: 'Update Succeed'
     else
       render :edit
@@ -44,7 +44,7 @@ class Admin::JobsController < ApplicationController
   private
 
   def job_params
-    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email)
+    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email, :is_hidden)
   end
 
 end
